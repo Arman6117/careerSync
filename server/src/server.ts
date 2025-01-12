1c39d58e8dde25840ee95e648d6b1de26bcf9b7f
Arman Patel <vanixgaming634@gmail.com>
2025-01-12 20:14:11 +0530

    refactor server.ts for code consistency and update root route response

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,16 +1,14 @@
-import express, { Application,Request,Response } from 'express'
+import express, { Application, Request, Response } from "express";
 
-const app:Application = express();
-const PORT = process.env.PORT || 5000
+const app: Application = express();
+const PORT = process.env.PORT || 5000;
 
-//!Middleware
-app.use(express.json())
+app.use(express.json());
 
-//!Routes
-app.get("/", (req:Request,res:Response)=> {
-    res.send("Typescript server is running")
-})
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hellow Node");
+});
 
-app.listen(PORT,()=>{
-    console.log(`Server is running on ${PORT}`)
-} )
+app.listen(PORT, () => {
+  console.log(`Server is running on ${PORT}`);
+});
