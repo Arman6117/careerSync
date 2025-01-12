4bc82bdd586957fd103d7e5bd108faf2e084fef5
Arman Patel <vanixgaming634@gmail.com>
2025-01-12 20:26:14 +0530

    add client components and utilities; implement Register page and Input component

---
+++ client/app/register/page.tsx
@@ -0,0 +1,14 @@
+'use client'
+
+import { Input } from "@/components/ui/input"
+
+const RegisterPage = () => {
+  return (
+<div>
+    Register page
+    <Input/>
+</div>
+)
+}
+
+export default RegisterPage
