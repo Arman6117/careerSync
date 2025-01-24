7a7790d54c5fdd2ae4b84eb55d4e0bcd96856bef
Arman Patel <vanixgaming634@gmail.com>
2025-01-24 13:31:37 +0530

    feat: enhance user authentication middleware; add user data to request and improve error handling for token verification

--- server/src/middleware/auth.middleware.ts
+++ server/src/middleware/auth.middleware.ts
@@ -1,31 +1,36 @@
-import { Request, Response, NextFunction } from 'express';
-import jwt from 'jsonwebtoken';
+import { Request, Response, NextFunction } from "express";
+import jwt, { JwtPayload } from "jsonwebtoken";
 
+interface AuthenticateRequest extends Request {
+  user?: jwt.JwtPayload | string;
+}
 export const authenticateUserMiddleware = (
-  req: Request, 
-  res: Response, 
+  req: AuthenticateRequest,
+  res: Response,
   next: NextFunction
 ): void => {
   try {
-    const token = req.headers.cookie?.split(";")
-      .find(cookie => cookie.trim().startsWith("accessToken="))
+    const token = req.headers.cookie
+      ?.split(";")
+      .find((cookie) => cookie.trim().startsWith("accessToken="))
       ?.split("=")[1];
 
     if (!token) {
       res.status(401).json({
         success: false,
-        message: "No token provided"
+        message: "No token provided",
       });
       return;
     }
 
     const secret = process.env.JWT_SECRET || "fallbackAccessTokenSecret";
-    jwt.verify(token, secret);
+    const decode = jwt.verify(token, secret);
+    req.user = decode as jwt.JwtPayload;
     next();
   } catch (error) {
-    res.status(401).json({ 
-      success: false, 
-      message: "Invalid or expired token!" 
+    res.status(401).json({
+      success: false,
+      message: "Invalid or expired token!",
     });
   }
 };
--- server/src/routes/user.routes.ts
+++ server/src/routes/user.routes.ts
@@ -4,10 +4,12 @@ import { authenticateUserMiddleware } from "../middleware/auth.middleware";
 const userRoutes = express.Router();
 
 userRoutes.get("/getUser", authenticateUserMiddleware, (req, res) => {
+   const user = (req as any).user
     res.status(200).json({
       success: true,
       message: "User authorized",
       // This comes from the middleware
+      user
     })});
 
 export default userRoutes;
