c0aad717f14eaa2fa6deb37aca0d2029762569a2
Arman Patel <vanixgaming634@gmail.com>
2025-01-27 15:43:02 +0530

    feat: enhance authentication middleware; add refresh token handling and improve error responses for expired tokens

--- server/src/middleware/auth.middleware.ts
+++ server/src/middleware/auth.middleware.ts
@@ -1,5 +1,6 @@
-import { Request, Response, NextFunction } from "express";
+import e, { Request, Response, NextFunction } from "express";
 import jwt, { JwtPayload } from "jsonwebtoken";
+import { refreshAccessToken } from "../utils/tokens";
 
 interface AuthenticateRequest extends Request {
   user?: jwt.JwtPayload | string;
@@ -15,6 +16,12 @@ export const authenticateUserMiddleware = (
       .find((cookie) => cookie.trim().startsWith("accessToken="))
       ?.split("=")[1];
 
+    const refreshToken = req.headers.cookie
+      ?.split(";")
+      .find((cookie) => cookie.trim().startsWith("refreshToken="))?.split("=")[1];
+      
+
+     
     if (!token) {
       res.status(401).json({
         success: false,
@@ -23,15 +30,27 @@ export const authenticateUserMiddleware = (
       return;
     }
 
+
     const secret = process.env.JWT_SECRET || "fallbackAccessTokenSecret";
-    const decode = jwt.verify(token, secret) as JwtPayload;
-    const expTime = decode.exp!;
-    const issuedTime = decode.iat!
-    const checkExp = new Date((expTime - issuedTime )* 1000).getMinutes()
-    console.log(checkExp)
-    req.user = decode as jwt.JwtPayload;
-    
-    next();
+    jwt.verify(token, secret, (err, decoded) => {
+      if (err?.name === "TokenExpiredError") {
+        if(!refreshToken) {
+          res.status(401).json({
+            success: false,
+            message: "Session expired please log in!",
+          });
+          return;
+        }
+        refreshAccessToken(refreshToken)
+      } else {
+        req.user = decoded as jwt.JwtPayload;
+        next();
+      }
+    });
+    res.status(401).json({
+      success: false,
+      message: "Invalid or expired token!",
+    });
   } catch (error) {
     res.status(401).json({
       success: false,
--- server/src/utils/tokens.ts
+++ server/src/utils/tokens.ts
@@ -3,18 +3,24 @@ export const generateAccessToken = (id: string) => {
   const accessToken = jwt.sign(
     { id },
     process.env.ACCESS_TOKEN_SECRET || "fallbackAccessTokenSecret",
-    { expiresIn: "1h" }
+    { expiresIn: "1m" }
   );
   return accessToken;
 };
 
-export const generateRefreshToken = (id:string) => {
-    const refreshToken = jwt.sign(
-        { id},
-        process.env.REFRESH_TOKEN_SECRET || "fallbackRefreshTokenSecret",
-  
-        { expiresIn: "7d" }
-      );
+export const generateRefreshToken = (id: string) => {
+  const refreshToken = jwt.sign(
+    { id },
+    process.env.REFRESH_TOKEN_SECRET || "fallbackRefreshTokenSecret",
 
-      return refreshToken;
-}
+    { expiresIn: "7d" }
+  );
+
+  return refreshToken;
+};
+
+export const refreshAccessToken = (refreshToken: string) => {
+  const secret =
+    process.env.REFRESH_TOKEN_SECRET || "fallbackRefreshTokenSecret";
+  const decoded = jwt.verify(refreshToken,secret);
+};
