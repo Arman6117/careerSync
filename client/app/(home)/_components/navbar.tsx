fa16636de4a72575f6ae67b148a5bcf2a6cf9603
Arman Patel <vanixgaming634@gmail.com>
2025-02-06 13:50:58 +0530

    feat: enhance navbar styling and layout; update logo size and add gradient effects

--- client/app/(home)/_components/navbar.tsx
+++ client/app/(home)/_components/navbar.tsx
@@ -8,13 +8,35 @@ const Navbar = () => {
   return (
     <nav className="flex justify-between">
       <Logo />
-      <div className="md:flex gap-3 hidden ">
-        <Link href={"/dashboard/jobs"}>Job</Link>
-        <Link href={"/dashboard/mock-interview"}>Ai Interview</Link>
+      <div className="md:flex gap-7 hidden mt-1">
+        <Link
+          className="font-semibold text-lg   transition-all hover:bg-gradient-to-b hover:bg-transparent hover:bg-clip-text hover:text-transparent from-pink-400 via-pink-500 to-pink-700 "
+          href={"/dashboard/jobs"}
+        >
+          Jobs
+        </Link>
+        <Link
+          className="font-semibold text-lg   transition-all hover:bg-gradient-to-b hover:bg-transparent hover:bg-clip-text hover:text-transparent from-pink-400 via-pink-500 to-pink-700 "
+          href={"/dashboard/mock-interview"}
+        >
+          Mock Interview
+        </Link>
       </div>
       <div className="flex gap-3">
-        <Button variant={"link"} className="font-semibold hover:text-[#fd6cff]">Login</Button>
-        <Button>Sign Up</Button>
+        <Button
+          variant={"outline"}
+          size={"lg"}
+          className="font-semibold text-lg  border-neutral-800 rounded-full transition-all hover:bg-gradient-to-b hover:bg-transparent hover:bg-clip-text hover:text-transparent from-pink-400 via-pink-500 to-pink-700  "
+        >
+          Login
+        </Button>
+        <Button
+          size={"lg"}
+          className="font-semibold text-lg text-white hover:bg-gradient-to-b hover:outline hover:outline-1 transition-all hover:outline-neutral-800  hover:bg-clip-text hover:text-transparent  rounded-full bg-gradient-to-r from-pink-400 via-pink-500 to-pink-600"
+        >
+          {" "}
+          Sign Up
+        </Button>
       </div>
     </nav>
   );
--- client/components/logo.tsx
+++ client/components/logo.tsx
@@ -4,8 +4,8 @@ import React from 'react'
 const Logo = () => {
   return (
     <div className="flex gap-3 items-center justify-center">
-        <Image src={"/logo.svg"} alt="logo" width={30} height={30} />
-        <div className="flex flex-col">
+        <Image src={"/logo.svg"} alt="logo" width={40} height={40} />
+        <div className="md:flex flex-col hidden">
           <span className="text-neutral-800 leading-5 font-bold text-lg ">
             Career-Sync
           </span>
