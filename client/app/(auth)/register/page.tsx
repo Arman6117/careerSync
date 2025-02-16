282c13495cab67fdd712f657f8d158ef69432a87
Arman Patel <vanixgaming634@gmail.com>
2025-02-16 19:53:11 +0530

    style: clean up formatting in RegisterPage component

--- client/app/(auth)/register/page.tsx
+++ client/app/(auth)/register/page.tsx
@@ -1,14 +1,10 @@
-"use client";
-import axios from "axios";
-import { Input } from "@/components/ui/input";
-import { Button } from "@/components/ui/button";
 import RegisterForm from "../_components/register-form";
 
 const RegisterPage = () => {
   return (
     <main className="relative flex items-center justify-center h-screen w-full">
-      <div  className="h-full absolute -z-10 w-full  pattern-zigzag-3d pattern-gray-600 pattern-opacity-5  pattern-bg-white pattern-size-32 "  ></div>
-      <RegisterForm/>
+      <div className="h-full absolute -z-10 w-full  pattern-zigzag-3d pattern-gray-600 pattern-opacity-5  pattern-bg-white pattern-size-32 "></div>
+      <RegisterForm />
     </main>
   );
 };
