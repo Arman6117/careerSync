279b363102a6209ae3612882f85343d3d034dc0b
Arman Patel <vanixgaming634@gmail.com>
2025-02-17 17:02:50 +0530

    feat: add Form component with context and hooks for improved form handling

--- client/app/(auth)/_components/login-form.tsx
+++ client/app/(auth)/_components/login-form.tsx
@@ -1,67 +1,105 @@
 "use client";
 import React, { useState } from "react";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
 
 import Link from "next/link";
+
+import { loginUser } from "@/actions/login";
 
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-import { loginUser } from "@/actions/login";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { zodResolver } from "@hookform/resolvers/zod";
 
 const LoginForm = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [isDisabled, setIsDisabled] = useState(false);
 
-  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    try {
-      loginUser({ email, password });
-    } catch (error) {
-      console.log(error);
-    }
-  };
+  const formSchema = z.object({
+    email: z.string().email(),
+    password: z.string().min(6).max(8),
+  });
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
   return (
     <div className="h-[400px] shadow-indigo-950  rounded-lg shadow-2xl border-r px-4 flex-col gap-6 w-[300px] flex bg-white bg-gradient-to-br from-white from-[80%]  to-pink-600 via-indigo-300">
       <h1 className="text-2xl font-semibold mt-10 text-center">Login </h1>
-      <form className="flex flex-col gap-4 " onSubmit={onSubmit}>
-        <div className="flex flex-col gap-2">
-          <Label className="text-sm font-semibold ml-1">Email</Label>
-          <Input
-            className="w-full border-gray-900 rounded-xl focus-visible:border-pink-600 focus-visible:ring-0 focus-visible:border-2 transition-all placeholder:text-xs"
-            placeholder="john@example.com"
-            type="email"
-            required
-            onChange={(e) => setEmail(e.target.value)}
+      <Form {...form}>
+        <form
+          className="flex flex-col gap-4 "
+          onSubmit={form.handleSubmit(() => {})}
+        >
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem className="flex flex-col gap-2">
+                <FormLabel className="text-sm font-semibold ml-1">
+                  Email
+                </FormLabel>
+                <FormControl>
+                  <Input
+                    className="w-full border-gray-900 rounded-xl focus-visible:border-pink-600 focus-visible:ring-0 focus-visible:border-2 transition-all placeholder:text-xs"
+                    placeholder="john@example.com"
+                    {...field}
+                  />
+                </FormControl>
+              </FormItem>
+            )}
           />
-        </div>
-        <div className="flex flex-col gap-2">
-          <Label className="text-sm font-semibold ml-1">Password</Label>
-          <Input
-            className="w-full border-gray-900 rounded-xl focus-visible:border-pink-600 focus-visible:ring-0 focus-visible:border-2 transition-all placeholder:text-xs"
-            placeholder="1234567"
-            required
-            maxLength={8}
-            minLength={6}
-            type="password"
-            onChange={(e) => setPassword(e.target.value)}
+          <FormField
+            control={form.control}
+            name="password"
+            render={({ field }) => (
+              <FormItem className="flex flex-col gap-2">
+                <FormLabel className="text-sm font-semibold ml-1">
+                  Password
+                </FormLabel>
+                <FormControl>
+                  <Input
+                    className="w-full border-gray-900 rounded-xl focus-visible:border-pink-600 focus-visible:ring-0 focus-visible:border-2 transition-all placeholder:text-xs"
+                    placeholder="12345678"
+                    {...field}
+                  />
+                </FormControl>
+              </FormItem>
+            )}
           />
-        </div>
-        <div className="flex justify-center">
-          <Button className="w-36 font-semibold  rounded-sm hover:bg-white hover:text-black transition-all hover:bg-clip-text hover:outline hover:outline-black bg-gradient-to-br from-indigo-200 from-[-3%] via-35% via-pink-500 to-[150%]  to-amber-400 text-white">
-            Login
-          </Button>
-        </div>
-        <div className="flex ga flex-col items-center ">
-          <span className="text-center text-xs">Already have an Account ?</span>
-          <Link
-            href={"/register"}
-            className="text-[11px] hover:underline text-pink-600"
-          >
-            Sign Up
-          </Link>
-        </div>
-      </form>
+
+          <div className="flex justify-center">
+            <Button className="w-36 font-semibold  rounded-sm hover:bg-white hover:text-black transition-all hover:bg-clip-text hover:outline hover:outline-black bg-gradient-to-br from-indigo-200 from-[-3%] via-35% via-pink-500 to-[150%]  to-amber-400 text-white">
+              Login
+            </Button>
+          </div>
+          <div className="flex ga flex-col items-center ">
+            <span className="text-center text-xs">
+              Already have an Account ?
+            </span>
+            <Link
+              href={"/register"}
+              className="text-[11px] hover:underline text-pink-600"
+            >
+              Sign Up
+            </Link>
+          </div>
+        </form>
+      </Form>
     </div>
   );
 };
