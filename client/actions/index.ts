fab004780105b2ad78918b252cfb4458d8fcd8e0
Arman Patel <vanixgaming634@gmail.com>
2025-02-20 20:54:19 +0530

    feat: implement Reset Password functionality with form and API integration

--- client/actions/index.ts
+++ client/actions/index.ts
@@ -62,16 +62,36 @@ export const verifyEmail = async ({ token }: { token: string }) => {
   }
 };
 
-export const sendForgotPassEmail = async ({email}:{email:string}) => {
-  const url = `${api}/api/auth/forgot-password`
+export const sendForgotPassEmail = async ({ email }: { email: string }) => {
+  const url = `${api}/api/auth/forgot-password`;
 
   try {
-    const {message,success} = (await axios.post(url, {email})).data
-    return {message,success}
+    const { message, success } = (await axios.post(url, { email })).data;
+    return { message, success };
   } catch (error) {
-    if(error instanceof AxiosError) {
-      return {message:error.response?.data.message,success:false}
+    if (error instanceof AxiosError) {
+      return { message: error.response?.data.message, success: false };
     }
-    return {message:"Something went wrong", success:false}
+    return { message: "Something went wrong", success: false };
   }
-}
+};
+export const resetUserPassword = async ({
+  token,
+  newPassword,
+}: {
+  token: string;
+  newPassword: string;
+}) => {
+  const url = `${api}/api/auth/reset-password`;
+
+  try {
+    const { message, success } = (await axios.post(url, { token, newPassword }))
+      .data;
+    return { message, success };
+  } catch (error) {
+    if (error instanceof AxiosError) {
+      return { message: error.response?.data.message, success: false };
+    }
+    return { message: "Something went wrong", success: false };
+  }
+};
