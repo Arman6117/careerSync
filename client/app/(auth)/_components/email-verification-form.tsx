09e0a372fcaa22bc8792af207f3f0d09296feae0
Arman Patel <vanixgaming634@gmail.com>
2025-02-21 19:44:00 +0530

    feat: add resend verification email functionality with input form

--- client/app/(auth)/_components/email-verification-form.tsx
+++ client/app/(auth)/_components/email-verification-form.tsx
@@ -12,13 +12,16 @@ import { toast } from "sonner";
 
 import { BsCheckCircleFill } from "react-icons/bs";
 import { RiLoader3Line } from "react-icons/ri";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import axios, { AxiosError } from "axios";
 
 const EmailVerificationForm = () => {
   const search = useSearchParams();
   const token = search.get("token")?.replace("/", "");
   const [isVerified, setIsVerified] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
-
+  const [email, setEmail] = useState("");
   if (!token) {
     return (
       <div className="h-[200px] shadow-indigo-950 items-center justify-center  rounded-lg shadow-2xl border-r px-4 flex-col gap-6 w-[300px] flex bg-white bg-gradient-to-br from-white from-[80%]  to-pink-600 via-indigo-300">
@@ -26,6 +29,25 @@ const EmailVerificationForm = () => {
       </div>
     );
   }
+
+  const handleResendEmail = async () => {
+    try {
+      const url = `${process.env.NEXT_PUBLIC_API_URL}/auth/resend-verification-email`;
+      const { message, success } = (await axios.post(url, { email })).data;
+      if (success) {
+        toast.success(message);
+      }
+      if (!success) {
+        toast.error(message);
+      }
+    } catch (error) {
+      if (error instanceof AxiosError) {
+        toast.error(error.response?.data.message);
+        return;
+      }
+      toast.error("Something went wrong");
+    }
+  };
   const onSubmit = useCallback(async () => {
     setIsLoading(true);
     try {
@@ -34,6 +56,30 @@ const EmailVerificationForm = () => {
       if (!success) {
         toast.error(message);
         return;
+      }
+      if (
+        (!success && message == "Link Expired") ||
+        message == "NoTokenError"
+      ) {
+        toast.error(message);
+        return (
+          <form
+            onSubmit={handleResendEmail}
+            className="h-[200px] shadow-indigo-950   justify-center items-center rounded-lg shadow-2xl border-r px-4 flex-col gap-6 w-[300px] flex bg-white bg-gradient-to-br from-white from-[80%]  to-pink-600 via-indigo-300"
+          >
+            <Input
+              // disabled={isLoading}
+              className="w-full border-gray-900 rounded-xl focus-visible:border-pink-600 focus-visible:ring-0 focus-visible:border-2 transition-all placeholder:text-xs"
+              placeholder="john@example.com"
+              type="email"
+              required
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <Button className="bg-pink-500 text-white" type="submit">
+              Submit
+            </Button>
+          </form>
+        );
       }
       if (success) {
         toast.success(message);
@@ -58,7 +104,7 @@ const EmailVerificationForm = () => {
             href={"/login"}
             className="text-[11px] hover:underline text-pink-600"
           >
-           Back To Login
+            Back To Login
           </Link>
         </div>
       ) : (
